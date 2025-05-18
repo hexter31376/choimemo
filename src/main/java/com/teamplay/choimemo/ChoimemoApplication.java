d6e2f7910922620e6f8a068403f97d0aff7883ca
hexter31376 <zwonyoung33@gmail.com>
2025-05-18 15:14:13 +0900

    initial commit

---
+++ src/main/java/com/teamplay/choimemo/ChoimemoApplication.java
@@ -0,0 +1,13 @@
+package com.teamplay.choimemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoimemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoimemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamplay/choimemo/ChoimemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamplay.choimemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoimemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
